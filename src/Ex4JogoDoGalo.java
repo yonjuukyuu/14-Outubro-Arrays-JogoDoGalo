820f0e46d86cd45edff37d59e55110408f59c497
Daniel Antonio Egidio Silva <a54072@ualg.pt>
2015-10-21 11:26:33 +0100

    Versao final com erros, Erro - 1 : Não esta a  validar as posições existentes bem Erro 2-  Não esta a encontrar um vencedor.

--- src/Ex4JogoDoGalo.java
+++ src/Ex4JogoDoGalo.java
@@ -27,14 +27,15 @@ public class Ex4JogoDoGalo {
 			
 			  	int jogadorA = 1;
 				int jogadorB = 2;
-		
-				System.out.println(" ");
-				System.out.println("Jogador A");
-				System.out.println(" ");
+				int	jogadorVencedor;
+				
 				int validarPosiçao = 0;
+				int jogadorAJogar = jogadorA;
 				
 				do	{
-					
+					System.out.println(" ");
+					System.out.println("Jogador A");
+					System.out.println(" ");
 					System.out.println("Introduza a Linha");
 					int jogarALinha = jogador.nextInt();
 					
@@ -49,18 +50,138 @@ public class Ex4JogoDoGalo {
 							if(m[jogarALinha][jogarAColuna] == jogadorA || m[jogarALinha][jogarAColuna] == jogadorB){
 								validarPosiçao = 0;
 								
-							}else
+							}else{
 								validarPosiçao = 1;
+								m[jogarALinha][jogarAColuna]  = jogadorA;
+								
+							}
 						}
 						
 						
 					}
 					
-				}while(validarPosiçao != 1);
+					if(m[0][0] == jogadorA && m[0][1] == jogadorA && m[0][2] == jogadorA ){
+						find = 1;
+						jogadorVencedor = jogadorA;
+					}else if(m[1][0] == jogadorA && m[1][1] == jogadorA && m[1][2] == jogadorA ){
+						find = 1;
+						jogadorVencedor = jogadorA;
+					}
+					else if(m[2][0] == jogadorA && m[2][1] == jogadorA && m[2][2] == jogadorA ){
+						find = 1;
+						jogadorVencedor = jogadorA;
+					}else if(m[0][0] == jogadorA && m[1][0] == jogadorA && m[2][0] == jogadorA ){
+						find = 1;
+						jogadorVencedor = jogadorA;
+					}
+					else if(m[0][1] == jogadorA && m[1][1] == jogadorA && m[2][1] == jogadorA ){
+						find = 1;
+						jogadorVencedor = jogadorA;
+					}else if(m[0][2] == jogadorA && m[1][2] == jogadorA && m[2][2] == jogadorA ){
+						find = 1;
+						jogadorVencedor = jogadorA;
+					}else if(m[0][0] == jogadorA && m[1][1] == jogadorA && m[2][2] == jogadorA ){
+						find = 1;
+						jogadorVencedor = jogadorA;
+					}
+					else if(m[0][2] == jogadorA && m[1][1] == jogadorA && m[2][0] == jogadorA ){
+						find = 1;
+						jogadorVencedor = jogadorA;
+					}
+					
+					
+					
+					
+				}while(validarPosiçao != 1 && jogadorAJogar != jogadorA);
 				
+				System.out.println("Matriz do Jogo Do Galo");
+				
+				for(int i = 0;i < M; i++){
+					System.out.println("");
+					for(int j = 0; j < N; j++){
+						System.out.print( m[i][j]);
+					}
+					
+					
+				}
+				
+				jogadorAJogar = jogadorB;
+				validarPosiçao = 0;
+				
+				do	{
+					System.out.println(" ");
+					System.out.println("Jogador B");
+					System.out.println(" ");
+					System.out.println("Introduza a Linha");
+					int jogarBLinha = jogador.nextInt();
+					
+					System.out.println("Introduza a Coluna");
+					int jogarBColuna = jogador.nextInt();
+					
+					
+					for(int i = 0;i < M; i++){
+						for(int j = 0; j < N; j++){
+						    // System.out.print( m[i][j]);
+							
+							if(m[jogarBLinha][jogarBColuna] == jogadorA || m[jogarBLinha][jogarBColuna] == jogadorB){
+								validarPosiçao = 0;
+								
+							}else{
+								validarPosiçao = 1;
+								m[jogarBLinha][jogarBColuna]  = jogadorB;
+								
+							}
+						}
+						
+						
+					}
+					if(m[0][0] == jogadorB && m[0][1] == jogadorB && m[0][2] == jogadorB ){
+						find = 1;
+						jogadorVencedor = jogadorB;
+					}else if(m[1][0] == jogadorB && m[1][1] == jogadorB && m[1][2] == jogadorB ){
+						find = 1;
+						jogadorVencedor = jogadorB;
+					}
+					else if(m[2][0] == jogadorB && m[2][1] == jogadorB && m[2][2] == jogadorB ){
+						find = 1;
+						jogadorVencedor = jogadorB;
+					}else if(m[0][0] == jogadorB && m[1][0] == jogadorB && m[2][0] == jogadorB ){
+						find = 1;
+						jogadorVencedor = jogadorB;
+					}
+					else if(m[0][1] == jogadorB && m[1][1] == jogadorB && m[2][1] == jogadorB ){
+						find = 1;
+						jogadorVencedor = jogadorA;
+					}else if(m[0][2] == jogadorB && m[1][2] == jogadorB && m[2][2] == jogadorB ){
+						find = 1;
+						jogadorVencedor = jogadorB;
+					}else if(m[0][0] == jogadorB && m[1][1] == jogadorB && m[2][2] == jogadorB){
+						find = 1;
+						jogadorVencedor = jogadorB;
+					}
+					else if(m[0][2] == jogadorB && m[1][1] == jogadorB && m[2][0] == jogadorB ){
+						find = 1;
+						jogadorVencedor = jogadorB;
+					}
+					
+				}while(validarPosiçao != 1 && jogadorAJogar != jogadorB);
+				
+				System.out.println("Matriz do Jogo Do Galo");
+				
+				for(int i = 0;i < M; i++){
+					
+					System.out.println("");
+					for(int j = 0; j < N; j++){
+						System.out.print( m[i][j]);
+					}
+					
+					
+				}
 			
 			}while( find != 1 );
 		
+		System.out.println("Jogo Do Galo END");
+
 	}
 	
 
